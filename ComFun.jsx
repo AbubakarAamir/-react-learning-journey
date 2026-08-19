132ab9282b51cc5c509ff5d805e119d65e61257f
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2026-08-19 13:03:16 -0700

    Add files via upload

---
+++ ComFun.jsx
@@ -0,0 +1,57 @@
+// Here we are Pracising after along time 
+
+function CompFun(){
+    return(
+        <>
+        <h1>Functional Components</h1>
+        <Provinces/>
+      <Names/>
+        </>
+    )
+}
+
+function Provinces(){
+    return(
+        <>
+        <h1>Provinces Names: </h1> 
+        <ul>
+            <li>lahore</li>
+            <li>quetta</li>
+            <li>pindi</li>
+            <li>karachi</li>
+        </ul>
+        </>
+    )
+}
+
+function Names(){
+    return(
+        <>
+        <h1>Names of Employees:</h1>
+        <ul>
+            <li>zain</li>
+            <li>ahmed</li>
+            <li>khan</li>
+        </ul>
+        </>
+    )
+}
+
+export function CountryNames(){
+    return(
+    <>
+    <h1>Coutry Names: </h1>
+ u;
+ <li>Pakistan</li>
+ <li>Saudi</li>
+ <li>German</li>
+    </>
+    )
+}
+
+
+
+
+
+
+export default CompFun;
