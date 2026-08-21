8c90cb78654bc527f50cf8edd476c92ec55dcfba
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2026-08-21 04:57:08 -0700

    Add files via upload

--- ToggleButton.jsx
+++ ToggleButton.jsx
@@ -10,7 +10,10 @@ function ToggleButton(){
         <>
         
         <h1>Toggle Button Practise (ON|OFF)</h1>
-        <button onClick={()=>setname(!name)} ON OR OFF></button>
+        <button onClick={()=>setname(!name)} ON OR OFF
+            style={{color:"blue", fontSize:10,borderRadius:10,cursor:"pointer",padding:10}}
+            
+            >Click me </button>
         <br />
         {
             // name?<h1>Abubakar Aamir</h1>:null
